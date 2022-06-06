5921e19e5ecad3b735f001dee5fe3e4aa6903cca
Litfa <admin@litf.com.cn>
2022-06-06 14:26:29 +0800

    fix: 修复路径拼接bug

--- src/utils/randomImg.ts
+++ src/utils/randomImg.ts
@@ -1,14 +1,14 @@
-import { join } from 'path'
+import { join, resolve } from 'path'
 import { readdirSync } from 'fs'
 import config from './config'
 import { CronJob } from 'cron'
 
 export default class RandomImg {
   private random = () => {
-    const dir = join(process.cwd(), config.imgUrl || './data/images/')
+    const dir = resolve(config.imgUrl || (process.cwd(), './data/images/'))
     const images = readdirSync(dir)
     const index = Math.floor(Math.random() * images.length)
-    return dir + images[index]
+    return join(dir, images[index])
   }
   private refresh = () => {
     this.path = this.random()
